82c37636f5892184c7f1117fb6c4af1ff064877b
KC <53473769+ksuchoi216@users.noreply.github.com>
2022-10-03 12:30:21 +0900

    pca is updated

--- utils/pca.py
+++ utils/pca.py
@@ -1,13 +1,22 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
-lsfrom sklearn.decomposition import PCA
+from sklearn.decomposition import PCA
 
 
-def execute_pca(numpy_data):
+def execute_pca(x, y_true):
+    sns.set(rc={'figure.figsize': (16, 10)})
+
+    if not isinstance(x, np.ndarray) or not isinstance(y_true, np.ndarray):
+        print(f'Please convert input data type to numpy array!!')
+        raise TypeError
+
+    print(x.shape, y_true.shape)
+
     scaler = StandardScaler()
-    scaler.fit(numpy_data)
-    standarized_data = scaler.transform(numpy_data)
+    scaler.fit(x)
+    standarized_data = scaler.transform(x)
 
     pca = PCA(n_components=2)
     pca.fit(standarized_data)
@@ -17,56 +26,12 @@ def execute_pca(numpy_data):
     pca_x1 = pca_x[:, 0]
     pca_x2 = pca_x[:, 1]
 
-    return pca_x1, pca_x2
+    data = np.column_stack((pca_x1, pca_x2, y_true))
 
+    df = pd.DataFrame(data=data, columns=['pca_x1', 'pca_x2', 'y_true'])
+    df.astype({'y_true': 'int8'})
 
-def show_pca(numpy_data_x, numpy_data_y, label_list=None):
-    train_x = numpy_data_x
-    train_y = numpy_data_y
+    df.info
 
-    # standarize features
-    scaler = StandardScaler()
-    scaler.fit(train_x)
-    standarized_x = scaler.transform(train_x)
-
-    pca = PCA(n_components=2)
-    pca.fit(standarized_x)
-
-    pca_x = pca.transform(standarized_x)
-    fig = plt.figure(1, figsize=(20, 10))
-
-    plot = plt.scatter(pca_x[:, 0], pca_x[:, 1], c=train_y)
-    plt.plot()
-
-    # plt.axis('off')
-    plt.legend(handles=plot.legend_elements()[0], labels=label_list)
-    plt.grid(True)
-    plt.show()
-
-
-def show_pca_with_prediction(x_emb, y_true, y_pred):
-    # standarize features
-    scaler = StandardScaler()
-    scaler.fit(x_emb)
-    standarized_x = scaler.transform(x_emb)
-    
-    pca = PCA(n_components=2)
-    pca.fit(standarized_x)
-
-    pca_x = pca.transform(standarized_x)
-    fig = plt.figure(1, figsize=(20, 10))
-    x1 = pca_x[:, 0]
-    x2 = pca_x[:, 1]
-
-    correction = np.where(y_true == y_pred, 1, 0)
-
-    df = pd.DataFrame(data=[x1, x2, y_true, y_pred, correction], columns=[
-                      "pca1", "pca2", "y_true", "y_pred", "correction"]).T
-
-    # sns.scatterplot(data = df, x=)
-    # plt.scatter(x1, x2, color='red', marker="x")
-
-    # plt.axis('off')
-    # plt.legend(handles = plot.legend_elements()[0], labels = label_list)
-    plt.grid(True)
-    plt.show()
+    sns.scatterplot(data=df, x='pca_x1', y='pca_x2',
+                    hue='y_true', style='y_true', palette='deep')
